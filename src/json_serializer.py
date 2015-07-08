8df5aec73b840c59c2cb64b5447f6d1cc6b5f668
Jack <z_chiz@live.com>
2015-07-08 13:52:18 -0700

    CLC-225: started encoder/decoder

--- src/json_serializer.py
+++ src/json_serializer.py
@@ -11,4 +11,22 @@ class FSMEncoder(json.JSONEncoder):
 
     def default(self, o):
         assert isinstance(o, FSM), 'Invalid argument type'
-        pass
+        json_out = {'states': o._states,
+                    'transitions': o._transitions,
+                    'initial_state_id': o.initial_state.id,
+                    'dead_state': o.dead_state}
+        return json_out
+
+class FSMDecoder(json.JSONDecoder):
+    """
+    To be able to reconstruct the FSM we need to retain:
+    States, transitions, initial state and dead state.
+    """
+
+    def default(self, o):
+        assert isinstance(o, FSM), 'Invalid argument type'
+        json_out = {'states': o._states,
+                    'transitions': o._transitions,
+                    'initial_state_id': o.initial_state.id,
+                    'dead_state': o.dead_state}
+        return json_out
